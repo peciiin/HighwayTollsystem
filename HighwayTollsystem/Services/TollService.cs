4ed8b9bde07dcad8b8820b9023d1b836bc1cdd90
peciiin <petr.elias3@seznam.cz>
2026-06-30 23:25:38 +0200

    Small update Added VignatteService 1/2 + Better TollService 1/2

--- HighwayTollsystem/Services/TollService.cs
+++ HighwayTollsystem/Services/TollService.cs
@@ -1,57 +1,60 @@
 ﻿using HighwayTollsystem.Models;
 using Microsoft.EntityFrameworkCore;
-
 namespace HighwayTollsystem.Services
 {
     public class TollService
     {
         private readonly HighwayTollContext _db;
-
         public TollService(HighwayTollContext db)
         {
             _db = db;
         }
 
-        public async Task ProcessPassageAsync(Passage passage)
+        public async Task PassageProcessingAsync(Passage passage)
         {
-            var vehicle = await _db.Vehicles
-                .Include(v => v.Type)
-                .FirstOrDefaultAsync(v => v.Spz == passage.Spz);
-
-
-
-            passage.IsVignetteValid = true;
-
-            // Uložíme průjezd do databáze
-            _db.Passages.Add(passage);
-            await _db.SaveChangesAsync();
-            int nadlimit = passage.VehicleSpeed - 130;
-
-            if (nadlimit > 0)
+            var vehicle = await _db.Vehicles.Include(t => t.Type).FirstOrDefaultAsync(x => x.Spz == passage.Spz);
+            if (vehicle == null)
             {
-                string violationCode = nadlimit switch
+                return;
+            }
+            CalculateRoadFee(passage, vehicle);
+
+
+
+        }
+
+        public async Task CalculateRoadFee(Passage passage, Vehicle vehicle)
+        {
+            passage.CalculatedFee = vehicle.Type.BaseTarif ?? 0.0m;
+        }
+
+        private async Task CheckViolations(Passage passage)
+        {
+            int speed = passage.VehicleSpeed - 130;
+            if (speed <= 0) return;
+
+            string code = speed switch
+            {
+                < 20 => "SPEED LOW",
+                < 50 => "SPEED MEDIUM",
+                _ => "SPEED HIGH"
+            };
+
+            var violationType = await _db.ViolationTypes
+                .FirstOrDefaultAsync(v => v.Code == code);
+
+            if (violationType != null)
+            {
+                var violation = new TrafficViolation
                 {
-                    >= 50 => "SPEED_HIGH",
-                    >= 20 => "SPEED_MEDIUM",
-                    _ => "SPEED_LOW"
+                    PassageId = passage.PassageId,
+                    ViolationTypeId = violationType.ViolationTypeId,
+                    Details = "Maximum speed: 130, violation: " + speed + " km/h",
+                    ActualPenaltyAmount = violationType.DefaultPenaltyAmount
                 };
 
-                var violationType = await _db.ViolationTypes
-                    .FirstOrDefaultAsync(vt => vt.Code == violationCode);
-
-                if (violationType != null)
-                {
-                    var violation = new TrafficViolation
-                    {
-                        PassageId = passage.PassageId,
-                        ViolationTypeId = violationType.ViolationTypeId,
-                        Details = "Speed exceded",
-                        ActualPenaltyAmount = violationType.DefaultPenaltyAmount
-                    };
-
-                    _db.TrafficViolations.Add(violation);
-                    await _db.SaveChangesAsync();
-                }
+                _db.TrafficViolations.Add(violation);
+                await _db.SaveChangesAsync();
             }
         }
     }
---
+++ HighwayTollsystem/Services/VignetteService.cs
@@ -0,0 +1,34 @@
+﻿using HighwayTollsystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HighwayTollsystem.Services
+{
+
+    public class VignetteService
+    {
+        private readonly HighwayTollContext _db;
+
+        public VignetteService(HighwayTollContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> IsVignetteValidAsync(Vehicle vehicle, DateTime passageTime)
+        {
+            
+            if (vehicle.Type.TypeName == "TRUCK")
+            {
+                return true;
+            }
+
+            
+            var activeVignette = await _db.Vignettes
+                .FirstOrDefaultAsync(v => v.Spz == vehicle.Spz &&
+                                          v.ValidFrom <= passageTime &&
+                                          v.ValidTo >= passageTime);
+
+            return activeVignette != null;
+        }
+    }
+}
