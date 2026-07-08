12137d70b757a30e3eec63127c6bb260ad9a512c
peciiin <petr.elias3@seznam.cz>
2026-07-08 23:11:26 +0200

    Stk check i will do it better, 2 parts emissons, technical

--- HighwayTollsystem/Services/StkService.cs
+++ HighwayTollsystem/Services/StkService.cs
@@ -1,10 +1,24 @@
-﻿namespace HighwayTollsystem.Services
+﻿using HighwayTollsystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HighwayTollsystem.Services
 {
     public class StkService
     {
-        public StkService() 
+        private readonly HighwayTollContext _db;
+        public StkService(HighwayTollContext db) 
         {
+            _db = db;
+        }
 
+        public async Task<bool> IsStkValidAsync(Vehicle vehicle, DateTime passageTime)
+        {
+            var activeStk = await _db.Stks
+                .FirstOrDefaultAsync(s => s.Spz == vehicle.Spz &&
+                                          s.ValidTo >= passageTime &&
+                                          s.EmissionsValidTo >= passageTime);
+
+            return activeStk != null;
         }
     }
 }
