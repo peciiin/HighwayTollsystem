222a3ed79cb590e9ec1711f75d6eaa5bb75a0738
peciiin <petr.elias3@seznam.cz>
2026-08-24 21:24:48 +0200

    .

--- HighwayTollsystem/Validators/RegisterTollPassDtoValidator.cs
+++ HighwayTollsystem/Validators/RegisterTollPassDtoValidator.cs
@@ -9,10 +9,10 @@ namespace HighwayTollsystem.Validators
             RuleFor(x => x.TollGateId)
                 .GreaterThan(0).WithMessage("TollGateId must be greater than 0.");
             RuleFor(x => x.DetectedSpz)
-                .NotEmpty().WithMessage("DetectedSpz is required.")
-                .Length(3,10).WithMessage("DetectedSpz cannot exceed 10 characters.");
+                .NotEmpty().WithMessage("Spz is required.")
+                .Length(3,10).WithMessage("Spz cannot exceed 10 characters.");
             RuleFor(x => x.VehicleSpeed)
-                .InclusiveBetween(0, 300).WithMessage("VehicleSpeed must be between 0 and 300.");
+                .InclusiveBetween(0, 300).WithMessage("Vehicle speed must be between 0 and 300.");
         }
     }
 }
