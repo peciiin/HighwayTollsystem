eba49e286f92f18ad21c220a06be345738ab8641
peciiin <petr.elias3@seznam.cz>
2026-09-08 22:58:46 +0200

    register toll pass validator complete

---
+++ HighwayTollsystem.Tests/Unit/Validator/RegisterNewVehicleDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using Org.BouncyCastle.Bcpg.Sig;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.TestHelper;
+using Xunit;
+using HighwayTollsystem.DTOs;
+using HighwayTollsystem.Validators;
+
+namespace HighwayTollsystem.Tests.Unit.Validator
+{
+    public class RegisterNewVehicleDtoValidator
+    {
+        private readonly RegisterNewVehicleDtoValidator _validator;
+
+        public RegisterNewVehicleDtoValidator()
+        {
+            _validator = new RegisterNewVehicleDtoValidator();
+        }
+
+    }
+}
--- HighwayTollsystem.Tests/Unit/Validator/RegisterTollPassDtoValidatorTests.cs
+++ HighwayTollsystem.Tests/Unit/Validator/RegisterTollPassDtoValidatorTests.cs
@@ -27,22 +27,28 @@ namespace HighwayTollsystem.Tests.Unit.Validator
         [InlineData(-50)]
         public void TollGateId_WhenZeroOrNegative_ShouldHaveValidationError(int gateId)
         {
-            var model = new RegisterTollPassDto { TollGateId = gateId };
+            var model = new RegisterTollPassDto 
+            {
+                TollGateId = gateId 
+            };
 
-            var result = _validator.TestValidate(model);
+            var res = _validator.TestValidate(model);
 
-            result.ShouldHaveValidationErrorFor(x => x.TollGateId)
+            res.ShouldHaveValidationErrorFor(x => x.TollGateId)
                   .WithErrorMessage("TollGateId must be greater than 0.");
         }
 
         [Fact]
         public void TollGateId_WhenGreaterThanZero_ShouldNotHaveValidationError()
         {
-            var model = new RegisterTollPassDto { TollGateId = 1 };
+            var model = new RegisterTollPassDto 
+            {
+                TollGateId = 1 
+            };
 
-            var result = _validator.TestValidate(model);
+            var res = _validator.TestValidate(model);
 
-            result.ShouldNotHaveValidationErrorFor(x => x.TollGateId);
+            res.ShouldNotHaveValidationErrorFor(x => x.TollGateId);
         }
 
         [Theory]
@@ -51,11 +57,13 @@ namespace HighwayTollsystem.Tests.Unit.Validator
         [InlineData(" ")]
         public void DetectedSpz_WhenEmptyOrNull_ShouldHaveValidationError(string? spz)
         {
-            var model = new RegisterTollPassDto { DetectedSpz = spz! };
+            var model = new RegisterTollPassDto 
+            { 
+                DetectedSpz = spz! 
+            };
+            var res = _validator.TestValidate(model);
 
-            var result = _validator.TestValidate(model);
-
-            result.ShouldHaveValidationErrorFor(x => x.DetectedSpz)
+            res.ShouldHaveValidationErrorFor(x => x.DetectedSpz)
                   .WithErrorMessage("Spz is required.");
         }
 
@@ -65,12 +73,54 @@ namespace HighwayTollsystem.Tests.Unit.Validator
         [InlineData("12345678901")]
         public void DetectedSpz_WhenLengthOutOfRange_ShouldHaveValidationError(string spz)
         {
-            var model = new RegisterTollPassDto { DetectedSpz = spz };
+            var model = new RegisterTollPassDto 
+            { 
+                DetectedSpz = spz 
+            };
 
-            var result = _validator.TestValidate(model);
+            var res = _validator.TestValidate(model);
 
-            result.ShouldHaveValidationErrorFor(x => x.DetectedSpz)
+            res.ShouldHaveValidationErrorFor(x => x.DetectedSpz)
                   .WithErrorMessage("Spz cannot exceed 10 characters.");
+        }
+
+        [Fact]
+        public void DetectedSpz_WhenValid_ShouldNotHaveValidationError()
+        {
+            var model = new RegisterTollPassDto 
+            { 
+                DetectedSpz = "ABC123" 
+            };
+            var res = _validator.TestValidate(model);
+            res.ShouldNotHaveValidationErrorFor(x => x.DetectedSpz);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(301)]
+        public void VehicleSpeed_WhenOutOfRange_ShouldHaveValidationError(int speed)
+        {
+            var model = new RegisterTollPassDto 
+            { 
+                VehicleSpeed = speed
+            };
+            var res = _validator.TestValidate(model);
+            res.ShouldHaveValidationErrorFor(x => x.VehicleSpeed)
+                  .WithErrorMessage("Vehicle speed must be between 0 and 300.");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(150)]
+        [InlineData(300)]
+        public void VehicleSpeed_WhenInRange_ShouldNotHaveValidationError(int speed)
+        {
+            var model = new RegisterTollPassDto 
+            { 
+                VehicleSpeed = speed 
+            };
+            var res = _validator.TestValidate(model);
+            res.ShouldNotHaveValidationErrorFor(x => x.VehicleSpeed);
         }
     }
 }
